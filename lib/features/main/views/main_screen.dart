4470dd927e5dfa826a8964abc50fc11cb4245e05
ijonghyeon <academy3746@gmail.com>
2024-03-20 22:20:47 +0900

    지워...

--- lib/features/main/views/main_screen.dart
+++ lib/features/main/views/main_screen.dart
@@ -16,7 +16,7 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
+class _MainScreenState extends State<MainScreen>{
   /// 뒤로가기 처리
   BackHandlerButton? backHandlerButton;
 
@@ -34,27 +34,7 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
   void initState() {
     super.initState();
 
-    WidgetsBinding.instance.addObserver(this);
-
     backHandlerButton = BackHandlerButton(context: context);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-
-    if (state == AppLifecycleState.resumed) {
-      print('앱이 포그라운드에서 실행중입니다.');
-    } else {
-      print('앱이 백그라운드에서 실행중입니다.');
-    }
   }
 
   @override
